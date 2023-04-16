4edb4eb514626c795f80cc8b92b48919190bad25
Mariya <mariyabutko@mail.ru>
2023-04-16 22:02:44 +0300

    LR8

---
+++ backend/front/src/App.js
@@ -0,0 +1,53 @@
+
+import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import NavigationBar from "./components/NavigationBar";
+import Home from "./components/Home";
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <NavigationBar />
+                <div className="container-fluid">
+                    <Routes>
+                        <Route path="home" element={<Home />}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
